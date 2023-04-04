1fce5dd811c9d32cbb93811418c195b1c0b214f9
Slipp Douglas Thompson <git@capnslipp.com>
2023-04-03 20:21:57 -0500

    WIP `makeVoxel3DTexturePaletted()` & `makePaletteTextureRGBA()`

--- MetalVoxelGeometryShader/Voxel3DTexture.swift
+++ MetalVoxelGeometryShader/Voxel3DTexture.swift
@@ -82,8 +82,8 @@ extension MTLDevice
 		for (x, yzArray): (Int, [[NSNumber]]) in voxelPaletteIndices.enumerated() {
 			for (y, zArray): (Int, [NSNumber]) in yzArray.enumerated() {
 				for (z, colorIndexNumber): (Int, NSNumber) in zArray.enumerated() {
-					let colorIndex = colorIndexNumber.intValue
-					let rgbaColor = paletteTextureColors[colorIndex]
+					let colorIndex = colorIndexNumber.uint8Value
+					let rgbaColor = paletteTextureColors[Int(colorIndex)]
 					rawData[(z * size.y * size.x) + (y * size.x) + x] = rgbaColor
 				}
 			}
@@ -93,6 +93,140 @@ extension MTLDevice
 			mipmapLevel: 0,
 			slice: 0,
 			withBytes: rawData,
+			bytesPerRow: size.x * MemoryLayout<Voxel3DTextureRGBAColor>.size,
+			bytesPerImage: (size.x * size.y) * MemoryLayout<Voxel3DTextureRGBAColor>.size
+		)
+		
+		guard let privateTexture = self.makeTexture(descriptor: with(descriptor){ $0.storageMode = .private }) else {
+			return nil
+		}
+		with(self.makeCommandQueue()!){ cq in
+			with(cq.makeCommandBufferWithUnretainedReferences()!){ cb in
+				with(cb.makeBlitCommandEncoder()!){ ce in
+					ce.copy(from: texture, to: privateTexture)
+					ce.endEncoding()
+				}
+				cb.commit()
+				cb.waitUntilCompleted()
+			}
+		}
+		
+		return privateTexture
+	}
+	
+	
+	func makeVoxel3DTexturePaletted(fromAsset asset: MDLVoxelAsset, model: MDLVoxelAssetModel? = nil) -> MTLTexture?
+	{
+		let model = model ?? asset.models.first!
+		
+		let size = withMap(model.voxelDimensions){ MTLSize(width: Int($0.x), height: Int($0.y), depth: Int($0.z)) }
+		
+		let descriptor = with(MTLTextureDescriptor.textureBufferDescriptor(
+			with: .r8Uint,
+			width: size.x,
+			usage: .shaderRead
+		)){ d in
+			d.textureType = .type3D
+			d.width = size.width
+			d.height = size.height
+			d.depth = size.depth
+			
+			d.cpuCacheMode = .writeCombined
+			d.storageMode = {
+				#if os(macOS)
+					return .managed
+				#else
+					return .shared
+				#endif
+			}()
+		}
+		
+		guard let texture = self.makeTexture(descriptor: descriptor) else {
+			return nil
+		}
+		
+		let voxelCount = Int(size.x * size.y * size.z)
+		
+		var rawData = [UInt8](
+			unsafeUninitializedCapacity: voxelCount,
+			initializingWith: { buffer, initializedCount in
+				buffer.initialize(repeating: 0)
+				initializedCount = voxelCount
+			}
+		)
+		let voxelPaletteIndices: [[[NSNumber]]] = model.voxelPaletteIndices;
+		for (x, yzArray): (Int, [[NSNumber]]) in voxelPaletteIndices.enumerated() {
+			for (y, zArray): (Int, [NSNumber]) in yzArray.enumerated() {
+				for (z, colorIndexNumber): (Int, NSNumber) in zArray.enumerated() {
+					let colorIndex = colorIndexNumber.uint8Value
+					rawData[(z * size.y * size.x) + (y * size.x) + x] = colorIndex
+				}
+			}
+		}
+		texture.replace(
+			region: MTLRegionMake3D(0, 0, 0, size.width, size.height, size.depth),
+			mipmapLevel: 0,
+			slice: 0,
+			withBytes: rawData,
+			bytesPerRow: size.x * MemoryLayout<UInt8>.size,
+			bytesPerImage: (size.x * size.y) * MemoryLayout<UInt8>.size
+		)
+		
+		guard let privateTexture = self.makeTexture(descriptor: with(descriptor){ $0.storageMode = .private }) else {
+			return nil
+		}
+		with(self.makeCommandQueue()!){ cq in
+			with(cq.makeCommandBufferWithUnretainedReferences()!){ cb in
+				with(cb.makeBlitCommandEncoder()!){ ce in
+					ce.copy(from: texture, to: privateTexture)
+					ce.endEncoding()
+				}
+				cb.commit()
+				cb.waitUntilCompleted()
+			}
+		}
+		
+		return privateTexture
+	}
+	
+	
+	func makePaletteTextureRGBA(fromAsset asset: MDLVoxelAsset) -> MTLTexture?
+	{
+		let paletteColorCount = 256
+		let size = MTLSize(width: paletteColorCount, height: 1, depth: 1)
+		
+		let descriptor = with(MTLTextureDescriptor.textureBufferDescriptor(
+			with: .rgba8Uint,
+			width: size.x,
+			usage: .shaderRead
+		)){ d in
+			d.textureType = .type1D
+			d.width = size.width
+			d.height = size.height
+			d.depth = size.depth
+			
+			d.cpuCacheMode = .writeCombined
+			d.storageMode = {
+				#if os(macOS)
+					return .managed
+				#else
+					return .shared
+				#endif
+			}()
+		}
+		
+		guard let texture = self.makeTexture(descriptor: descriptor) else {
+			return nil
+		}
+		
+		let paletteTextureColors = asset.paletteColors.map(Voxel3DTextureRGBAColor.from(color:))
+		assert(paletteTextureColors.count == paletteColorCount)
+		
+		texture.replace(
+			region: MTLRegionMake3D(0, 0, 0, size.width, size.height, size.depth),
+			mipmapLevel: 0,
+			slice: 0,
+			withBytes: paletteTextureColors,
 			bytesPerRow: size.x * MemoryLayout<Voxel3DTextureRGBAColor>.size,
 			bytesPerImage: (size.x * size.y) * MemoryLayout<Voxel3DTextureRGBAColor>.size
 		)
